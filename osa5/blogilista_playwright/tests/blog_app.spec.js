06ed7c0e9f1e319e4debbc63df934fae0cd1d929
ehirvi <eino.hirvi@gmail.com>
2024-06-07 12:31:24 +0300

    5.17 & 5.18

--- osa5/blogilista_playwright/tests/blog_app.spec.js
+++ osa5/blogilista_playwright/tests/blog_app.spec.js
@@ -5,16 +5,41 @@ describe('Blog app', () => {
     await request.post('http://localhost:3003/api/testing/reset')
     await request.post('http://localhost:3003/api/users', {
       data: {
-        name: 'user',
-        username: 'user',
-        password: 'user'
+        name: 'human',
+        username: 'nameuser',
+        password: 'wordpass'
       }
     })
     await page.goto('http://localhost:5173')
   })
 
   test('Login form is shown', async ({ page }) => {
-    const locator = await page.getByText('Log in to application')
-    await expect(locator).toBeVisible()
+    const headerLocator = page.getByText('Log in to application')
+    const usernameLocator = page.locator('input[type="text"]')
+    const passwordLocator = page.locator('input[type="password"]')
+    await expect(headerLocator).toBeVisible()
+    await expect(usernameLocator).toBeVisible()
+    await expect(passwordLocator).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.locator('input[type="text"]').fill('nameuser')
+      await page.locator('input[type="password"]').fill('wordpass')
+      await page.getByRole('button', { name: 'Login' }).click()
+      const locator = page.getByText('human logged in')
+      await expect(locator).toBeVisible()
+    })
+
+    test('fails with incorrect credentials', async ({ page }) => {
+      await page.locator('input[type="text"]').fill('pekka')
+      await page.locator('input[type="password"]').fill('pekkanen')
+      await page.getByRole('button', { name: 'Login' }).click()
+      const errorNotification = page.locator('.error')
+      await expect(errorNotification).toContainText('Wrong username or password')
+      await expect(errorNotification).toHaveCSS('border-style', 'solid')
+      await expect(errorNotification).toHaveCSS('color', 'rgb(255, 0, 0)')
+      await expect(page.getByText('pekka logged in')).not.toBeVisible()
+    })
   })
 })
