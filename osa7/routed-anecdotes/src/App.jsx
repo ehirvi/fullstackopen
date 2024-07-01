51028be24a7e8adf531a689297a3cda5b93a6001
ehirvi <eino.hirvi@gmail.com>
2024-07-01 16:45:01 +0300

    7.5

--- osa7/routed-anecdotes/src/App.jsx
+++ osa7/routed-anecdotes/src/App.jsx
@@ -88,6 +88,12 @@ const CreateNew = (props) => {
     })
   }
 
+  const resetForm = () => {
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -104,7 +110,8 @@ const CreateNew = (props) => {
           url for more info
           <input name='info' {...info} />
         </div>
-        <button>create</button>
+        <button type='submit'>create</button>
+        <button type='button' onClick={resetForm}>reset</button>
       </form>
     </div>
   )
