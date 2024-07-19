1df506e1a32e63bf13780c50454bc74ad8ead5dc
ehirvi <eino.hirvi@gmail.com>
2024-07-19 19:48:27 +0300

    8.2

--- osa8/kirjasto/index.js
+++ osa8/kirjasto/index.js
@@ -93,10 +93,6 @@ let books = [
   },
 ];
 
-/*
-  you can remove the placeholder query once your first one has been implemented 
-*/
-
 const typeDefs = `
   type Author {
     name: String!
@@ -107,7 +103,7 @@ const typeDefs = `
   type Book {
     title: String!
     published: Int!
-    author: Author!
+    author: String!
     id: ID!
     genres: [String!]!
   }
@@ -115,13 +111,15 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `;
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   },
 };
 
