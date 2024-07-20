3298833088a8b12bb2f000b6d423f596e7999233
ehirvi <eino.hirvi@gmail.com>
2024-07-20 11:29:59 +0300

    8.4

--- osa8/kirjasto/index.js
+++ osa8/kirjasto/index.js
@@ -97,7 +97,7 @@ const typeDefs = `
   type Author {
     name: String!
     born: Int
-    bookCount: Int
+    bookCount: Int!
     id: ID!
   }
 
@@ -112,7 +112,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -121,15 +121,20 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
-    allAuthors: () => authors
+    allBooks: (root, args) => {
+      if (args.author) {
+        return books.filter((b) => b.author === args.author);
+      }
+      return books;
+    },
+    allAuthors: () => authors,
   },
 
   Author: {
     bookCount: (root) => {
-      return books.filter(b => b.author === root.name).length
-    }
-  }
+      return books.filter((b) => b.author === root.name).length;
+    },
+  },
 };
 
 const server = new ApolloServer({
