e107aeae2b5e5f3983764a1286c373cf425ee45b
ehirvi <eino.hirvi@gmail.com>
2024-08-17 17:49:10 +0300

    9.5

--- osa9/calculators/index.ts
+++ osa9/calculators/index.ts
@@ -1,8 +1,33 @@
 import express from "express";
+import bmiCalculator from "./bmiCalculator";
+import { isNotNumber } from "./utils";
 const app = express();
+
+app.set("query parser", "extended");
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
+});
+
+app.get("/bmi", (req, res) => {
+  if (!req.query.height || !req.query.weight) {
+    return res.status(400).json({ error: "malformatted parameters" });
+  }
+
+  if (isNotNumber(req.query.height) || isNotNumber(req.query.weight)) {
+    return res.status(400).json({ error: "malformatted parameters" });
+  }
+
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
+
+  const bmi = bmiCalculator.calculateBmi(height, weight);
+  const result = {
+    weight,
+    height,
+    bmi,
+  };
+  return res.json(result);
 });
 
 const PORT = 3003;
