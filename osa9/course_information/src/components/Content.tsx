c523aca8a5a9e88dd9a865ac27a7d3634c99b80c
ehirvi <eino.hirvi@gmail.com>
2024-09-07 13:16:32 +0300

    9.15

---
+++ osa9/course_information/src/components/Content.tsx
@@ -0,0 +1,22 @@
+interface CoursePart {
+  name: string;
+  exerciseCount: number;
+}
+
+interface ContentProps {
+  courseParts: CoursePart[];
+}
+
+const Content = (props: ContentProps) => {
+  return (
+    <>
+      {props.courseParts.map((part) => (
+        <p key={part.name}>
+          {part.name} {part.exerciseCount}
+        </p>
+      ))}
+    </>
+  );
+};
+
+export default Content;
