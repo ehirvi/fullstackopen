6a1b5798b9b026597f737a6e524f9a0c1c32156e
ehirvi <eino.hirvi@gmail.com>
2024-09-17 12:12:55 +0300

    9.23

--- osa9/patientor-backend/src/types/types.ts
+++ osa9/patientor-backend/src/types/types.ts
@@ -7,14 +7,53 @@ export interface Diagnosis {
   latin?: string;
 }
 
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
 export enum Gender {
   Male = "male",
   Female = "female",
   Other = "other",
 }
-
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Entry {}
 
 export interface Patient {
   id: string;
